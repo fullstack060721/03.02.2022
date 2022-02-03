56e5c6c1dc5f7532f7b7389f4e64291dae3e7c23
micserv010821 <dotnet290320@gmail.com>
2022-02-03 18:51:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const path = require('path')
 const knex = require('knex')
 
 const app = express()
+
+const connectedKnex = knex({
+    client: "sqlite3",
+    connection: {
+        filename: "employee.db"
+    }
+})
+
+app.use(express.static(path.join(__dirname, '/')))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+});
